ba5d449991be9b6192e1ab99cda8ff126ce30076
Kavya-uday123 <mca522@rajagiri.edu>
2021-05-21 21:48:21 +0530

    Office Chair Project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,15 +6,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>aranoz</title>
-    <link rel="icon" href="../img/favicon.png">
+    <link rel="icon" href="<../img/favicon.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="<../css/bootstrap.min.css">
     <!-- animate CSS -->
-    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="<../css/animate.css">
     <!-- owl carousel CSS -->
-    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<../css/owl.carousel.min.css">
     <!-- font awesome CSS -->
-    <link rel="stylesheet" href="../css/all.css">
+    <link rel="stylesheet" href="<../css/all.css">
     <!-- flaticon CSS -->
     <link rel="stylesheet" href="<../css/flaticon.css">
     <link rel="stylesheet" href="<../css/themify-icons.css">
