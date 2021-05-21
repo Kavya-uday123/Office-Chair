3af5470a265f0749e35451272c7c1ba1e9a85fdd
Kavya-uday123 <mca522@rajagiri.edu>
2021-05-21 23:43:32 +0530

    Office Chair Project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,24 +6,24 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>aranoz</title>
-    <link rel="icon" href="<../img/favicon.png">
+    <link rel="icon" href="../img/favicon.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- animate CSS -->
-    <link rel="stylesheet" href="<../css/animate.css">
+    <link rel="stylesheet" href="../css/animate.css">
     <!-- owl carousel CSS -->
-    <link rel="stylesheet" href="<../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
     <!-- font awesome CSS -->
-    <link rel="stylesheet" href="<../css/all.css">
+    <link rel="stylesheet" href="../css/all.css">
     <!-- flaticon CSS -->
-    <link rel="stylesheet" href="<../css/flaticon.css">
-    <link rel="stylesheet" href="<../css/themify-icons.css">
+    <link rel="stylesheet" href="../css/flaticon.css">
+    <link rel="stylesheet" href="../css/themify-icons.css">
     <!-- font awesome CSS -->
-    <link rel="stylesheet" href="<../css/magnific-popup.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
     <!-- swiper CSS -->
     <link rel="stylesheet" href="../css/slick.css">
     <!-- style CSS -->
-    <link rel="stylesheet" href="<../css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <script>
     function getlogin()
     {
@@ -40,7 +40,7 @@
             <div class="row align-items-center">
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg navbar-light">
-                        <a class="navbar-brand" href="/"> <img src="<?php echo url('/'); ?>/img/logo.png" alt="logo"> </a>
+                        <a class="navbar-brand" href="/"> <img src="../img/logo.png" alt="logo"> </a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse"
                             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -501,34 +501,34 @@ https://www.featherlitefurniture.com/drupal/sites/default/files/pictures/04_Opti
             <div class="row align-items-center">
                 <div class="col-lg-12">
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_1.png" alt="">
+                        <img src="../img/client_logo/client_logo_1.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_2.png" alt="">
+                        <img src="../img/client_logo/client_logo_2.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_3.png" alt="">
+                        <img src="../img/client_logo/client_logo_3.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_4.png" alt="">
+                        <img src="../img/client_logo/client_logo_4.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_5.png" alt="">
+                        <img src="../img/client_logo/client_logo_5.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_3.png" alt="">
+                        <img src="../img/client_logo/client_logo_3.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_1.png" alt="">
+                        <img src="../img/client_logo/client_logo_1.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_2.png" alt="">
+                        <img src="../img/client_logo/client_logo_2.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_3.png" alt="">
+                        <img src="../img/client_logo/client_logo_3.png" alt="">
                     </div>
                     <div class="single_client_logo">
-                        <img src="<?php echo url('/'); ?>/img/client_logo/client_logo_4.png" alt="">
+                        <img src="../img/client_logo/client_logo_4.png" alt="">
                     </div>
                 </div>
             </div>
