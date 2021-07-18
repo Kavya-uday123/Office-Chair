d320e414ae3ac8114bc8cbc458a7582a6916d33e
Kavya-uday123 <mca522@rajagiri.edu>
2021-07-18 17:40:09 +0530

    Office Chair

--- app/Http/Controllers/ChairControllerlog.php
+++ app/Http/Controllers/ChairControllerlog.php
@@ -47,7 +47,7 @@ class ChairControllerlog extends Controller
         $request->validate([
             'Name'=>'required',
             'Address'=>'required',
-            'Pincode'=>'required|numeric',
+            'Pincode'=>'required|numeric|digits:6',
             'State'=>'required',
             'Gender'=>'required',
             'Phone'=>'required|numeric|digits:10',
